bb2bd43f845b9e71ad087256e3aaafa5d1d30eb5
xoXen <bitesyouzxc@gmail.com>
2023-03-07 16:49:51 +0800

    Test2

--- Project Codes/index.php
+++ Project Codes/index.php
@@ -5,7 +5,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
 -->
 <html>
     <head>
-        <title>World of Pets</title>
+        <title>Paws & Claws</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet"
