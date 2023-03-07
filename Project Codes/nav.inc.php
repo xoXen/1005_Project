acbcdc7b72f0a82158d6cdb1805677dae407e336
Clarabel Teo <hackedbyskittles@gmail.com>
2023-03-07 17:13:57 +0800

    added new pages

--- Project Codes/nav.inc.php
+++ Project Codes/nav.inc.php
@@ -1,24 +1,38 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">
-       <img src="Images/Logo.jpg" class=".img-fluid" width="100" alt="Logo"/>
-    </a>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="register.php">Register</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php#dogs">Dogs</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php#cats">Cats</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html">
+                <img class="logo" src="images/smudgelogo.jpg" width="50" alt="websitelogo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Paws & Claws</a>
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                       <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About us</a>
+                    <li class ="nav-item">
+                        <a class="nav-link" href="services.php">Our services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="book.php">Book Appointment</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="faq.php">FAQ</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">login logo</a> 
+                    </li>
+                    
+
+
+                </ul>
+
+            </div>
+        </nav>
