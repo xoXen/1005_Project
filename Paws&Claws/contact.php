3c1bce54a3eadc6d79c744bb11dfd6bcfcac0290
Clarabel Teo <hackedbyskittles@gmail.com>
2023-03-14 16:15:56 +0800

    added contact.php and process_contact.php
    not working yet, just for testing

--- Paws&Claws/contact.php
+++ Paws&Claws/contact.php
@@ -1,7 +1,76 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
+-->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <!--jQuery-->
+        <script defer
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+        </script>
+        <!-- Custom JS -->
+        <script defer src="js/main.js"></script>
+        <script defer src="js/contact.js"></script>
+        <!--Bootstrap JS-->
+        <script defer
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
+            integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
+            crossorigin="anonymous">
+        </script>
+        <link rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity=
+        "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="css/contact.css">
+        <title>Paws & Claws</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+        include "nav.inc.php";
+        ?>
+        <main class="container">
+            <h1>Contact Us</h1>
+            <p>
+                You may also call the clinic at 6123 4567 or drop us an <a href = mailto: pawsnclaws@gmail.com">email</a>
+
+            </p>
+            <form action="process_register.php" method="post">
+
+            <div class="form-group">
+            <label for="name">Name:</label>
+            <input class="form-control" type="text" id="name" required maxlength="45" name="name"
+                placeholder="Enter your name">
+            </div>
+            <div class="form-group">
+            <label for="email">Email:</label>
+            <input class="form-control" type="email" id="email" required name="email"
+                placeholder="Enter email">
+            </div>
+            <div class="form-group">
+            <label for="text">Message:</label>
+            <input class="form-control" type="text" id="msg" required name="msg"
+                placeholder="Enter message">
+            </div>
+                
+            <div class="form-check">
+                <label>
+                    <input type="checkbox" required name="agree">
+                    I <strong>Agree</strong> to be contacted at the above email address.
+                </label>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Send Message</button>
+            </div>
+        </form>
+</main>
 <?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+include "footer.inc.php";
+?>
+</body>
---
+++ Paws&Claws/process_contact.php
@@ -0,0 +1,157 @@
+<?php
+$email = $errorMsg = "";
+$success = true;
+
+
+$emailcheck = empty(trim($_POST["email"]));
+
+$namecheck = empty(trim($_POST["name"]));
+$namelengthcheck = strlen(trim($_POST["name"]));
+$messagelengthcheck = strlen($_POST["message"]);
+
+
+$msgcheck = empty(trim($_POST["msg"]));
+
+if ($emailcheck)
+    {
+        $errorMsg .= "Email is required.<br>";
+        $success = false;
+    }
+else {
+    if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+        $errorMsg .= "Invalid Email Format. <br>";
+        $success = false;
+    }
+    else{
+        $email = sanitize_input($_POST["email"]);
+    }
+}
+
+if ($namecheck){
+    $errorMsg .= "Name is required. <br>";
+    $success = false;
+}
+else if($namelengthcheck > 45){
+    $errorMsg .= "Name can only have a maximum of 45 characters. <br>";
+    $success = false;
+}
+else{
+    $fname = sanitize_input($_POST["name"]);
+}
+
+
+
+
+if ($msgcheck) {
+    $errorMsg .= "Message cannot be empty. <br>";
+    $success = false;
+}
+else if($messagelengthcheck > 45){
+    $errorMsg .= "Message can only be a maximum of 2000 characters. <br>";
+    $success = false;
+}
+else{
+    $fname = sanitize_input($_POST["message"]);
+}
+
+
+if($success){
+    saveMemberToDB($message);
+//    echo $_POST['pwd'];
+}
+
+
+
+//Helper function that checks input for malicious or unwanted content.
+function sanitize_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    
+/*
+* Helper function to write the member data to the DB
+*/
+function saveMemberToDB($message)
+{
+    global $name, $email, $message, $errorMsg, $success;
+    // Create database connection.
+    $config = parse_ini_file('../../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+    // Check connection
+    if ($conn->connect_error)
+    {
+
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        $success = false;
+    }
+    else
+    {
+        // Prepare the statement:
+        $stmt = $conn->prepare("INSERT INTO pnc_contact (name, email, message) VALUES (?, ?, ?)");
+        // Bind & execute the query statement:
+        $stmt->bind_param("ssss", $name, $email, $message);
+        if (!$stmt->execute())
+        {
+            $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+            $success = false;
+        }
+        $stmt->close();
+    }
+    $conn->close();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <meta charset="utf-8">
+
+        <!--jQuery-->
+        <script defer
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+        </script>
+        <!-- Custom JS -->
+        <script defer src="js/main.js"></script>
+        <script defer src="js/contact.js"></script>
+        <!--Bootstrap JS-->
+        <script defer
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
+            integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
+            crossorigin="anonymous">
+        </script>
+        <link rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity=
+        "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="css/contact.css">
+        <title>Paws & Claws</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php include "nav.inc.php";?>
+        <?php
+        if ($success) {
+            echo " <br><br><main class='container'>";
+            echo "<h3>Thank you for contacting us, " . $name .". We will get back to you shortly</h4>";
+            echo "<form action='index.php'>";
+            echo "<input class='btn btn-success' type='submit' value='Home' /></form><br><br>";
+        } else {
+            echo " <br><br><main class='container'>";
+            echo "<h1> Oops! </h1>";
+            echo "<h3>The following input errors were detected:</h4>";
+            echo "<p>" . $errorMsg . "</p>";
+            echo "<form action='contact.php'>";
+            echo "<input class='btn btn-danger' type='submit' value='Return to Contact' /></form><br><br>";
+        }
+        ?>
+    </body>
+    <?php include "footer.inc.php";?>
+</html>
