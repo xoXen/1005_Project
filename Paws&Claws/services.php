005b4eec053fefa0d5d2e40e8ddb3770dc612200
Clarabel Teo <hackedbyskittles@gmail.com>
2023-03-14 16:43:47 +0800

    added services.php
    wip

--- Paws&Claws/services.php
+++ Paws&Claws/services.php
@@ -1,101 +1,86 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
--->
-<!--test-->
 <html>
     <head>
-        <title>Paws & Claws!</title>
+        <title>Paws & Claws! </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity=
-        "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossorigin="anonymous">
-        <link rel="stylesheet" href="CSS/main.css">
-        <!--jQuery-->
+        <link rel="stylesheet"              href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity=
+              "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+              crossorigin="anonymous">
+        <link rel="stylesheet" href="CSS/services.css">
+                <style data-href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&amp;family=DM+Serif+Display&amp;display=swap">@font-face{font-family:'DM Serif Display';font-style:normal;font-weight:400;font-display:swap;src:url(https://fonts.gstatic.com/s/dmserifdisplay/v11/-nFnOHM81r4j6k0gjAW3mujVU2B2K_Q.woff) format('woff')}@font-face{font-family:'Lato';font-style:normal;font-weight:400;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6uyw4BMUTPHvxo.woff) format('woff')}@font-face{font-family:'Lato';font-style:normal;font-weight:700;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6u9w4BMUTPHh6UVeww.woff) format('woff')}@font-face{font-family:'Lato';font-style:normal;font-weight:900;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6u9w4BMUTPHh50Xeww.woff) format('woff')}@font-face{font-family:'DM Serif Display';font-style:normal;font-weight:400;font-display:swap;src:url(https://fonts.gstatic.com/s/dmserifdisplay/v11/-nFnOHM81r4j6k0gjAW3mujVU2B2G_5x0vrx52jJ3Q.woff2) format('woff2');unicode-range:U+0100-024F,U+0259,U+1E00-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF}@font-face{font-family:'DM Serif Display';font-style:normal;font-weight:400;font-display:swap;src:url(https://fonts.gstatic.com/s/dmserifdisplay/v11/-nFnOHM81r4j6k0gjAW3mujVU2B2G_Bx0vrx52g.woff2) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}@font-face{font-family:'Lato';font-style:normal;font-weight:400;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6uyw4BMUTPHjxAwXiWtFCfQ7A.woff2) format('woff2');unicode-range:U+0100-024F,U+0259,U+1E00-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF}@font-face{font-family:'Lato';font-style:normal;font-weight:400;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6uyw4BMUTPHjx4wXiWtFCc.woff2) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}@font-face{font-family:'Lato';font-style:normal;font-weight:700;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6u9w4BMUTPHh6UVSwaPGQ3q5d0N7w.woff2) format('woff2');unicode-range:U+0100-024F,U+0259,U+1E00-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF}@font-face{font-family:'Lato';font-style:normal;font-weight:700;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6u9w4BMUTPHh6UVSwiPGQ3q5d0.woff2) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}@font-face{font-family:'Lato';font-style:normal;font-weight:900;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6u9w4BMUTPHh50XSwaPGQ3q5d0N7w.woff2) format('woff2');unicode-range:U+0100-024F,U+0259,U+1E00-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF}@font-face{font-family:'Lato';font-style:normal;font-weight:900;font-display:swap;src:url(https://fonts.gstatic.com/s/lato/v23/S6u9w4BMUTPHh50XSwiPGQ3q5d0.woff2) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}</style>
+                
+                        <!--jQuery-->
         <!-- defer tells the browser to wait until complete webpage has been 
            loaded and parsed before loading javascript -->
         <script defer
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous">
+                src="https://code.jquery.com/jquery-3.4.1.min.js"
+                integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+                crossorigin="anonymous">
         </script>
-        <!--Bootstrap JS-->
+        
+          <!--Bootstrap JS-->
         <script defer
-            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-            integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
-            crossorigin="anonymous">
+                src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
+                integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
+                crossorigin="anonymous">
         </script>
         <!-- Custom JS -->
         <script defer src="JS/main.js"></script>
     </head>
     <body>
-        <?php include "nav.inc.php" ?>
+        <?php include "PHP/nav.inc.php" ?>
         <!-- expand-[size] -->
         <!--jumbotron is a big box used to call for extra attention-->
-        <header class="jumbotron text-center">
-            <h1 class="display-4">Welcome to World of Pets!</h1>
-            <h2>Home of Singapore's Pet Lovers</h2>
-        </header>
-        <main class="container">
-            <section id="dogs">
-                <h2>All About Dogs!</h2>
+
+        
+        <main>
+            <div class="container-fluid">
                 <div class="row">
-                    <article class="col-sm">
-                        <h3>Poodles</h3>
-                        <figure>
-                            <img class="img-thumbnail" src="Images/poodle_small.jpg" alt="Poodle"/>
-                            <figcaption>Standard Poodle</figcaption>
-                        </figure>
-                        <p>
-                            Poodles are a group of formal dog breeds, the Standard
-                            Poodle, Miniature Poodle and Toy Poodle...
-                        </p>
-                    </article>
-                    <article class="col-sm">
-                        <h3>Chihuahua</h3>
-                        <figure>
-                            <img class="img-thumbnail" src="Images/chihuahua_small.jpg" alt="Poodle"/>
-                            <figcaption>Chihuahua</figcaption>
-                        </figure>
-                        <p>
-                            The Chihuahua is the smallest breed of dog, and is named
-                            after the Mexican state of Chihuahua...
-                        </p>
-                    </article>
+
+                    <div class="col-md-6">
+                        <div class="flex-1 text-center col-lg   " id="rightbox">
+                            <h1 class="h1services">Health Check Ups</h1>
+                            <p class="">Starting from $35</p>
+                        <div class="">
+                            <p class="">Book a consultation with a professional licensed vet from the comfort of your home. Receive a custom treatment plan and medication prescription for your pet's health condition at an affordable rate.</p>
+                        </div>
+                            <a class="" href="https://zumvet.com/app/booking">Book now</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/routinecheckup.jpg" width="100%" height="100%" class="img-responsive" alt="header image of dog">
+                    </div>
                 </div>
-            </section>
-            <section id="cats">
-                <h2>All About Cats!</h2>
+            </div>
+
+        </div>
+
+
+        <header class="jumbotron text-center">
+            <h1 class="display-1">Services</h1>
+            <h2>We offer a wide range of services for you and your pet</h2>
+        </header>
+
+            <h2>Routine Checkups</h2>
+
+
+            <!--</section>-->
+            <section id="vacc">
+                <h2>Vaccinations</h2>
                 <div class="row">
-                    <article class="col-sm">
-                        <h3>Tabby</h3>
-                        <figure>
-                            <img class="img-thumbnail" src="Images/tabby_small.jpg" alt="Poodle"/>
-                            <figcaption>Tabby Cat</figcaption>
-                        </figure>
+
+                    <article class="col-lg">
+
                         <p>
                             A tabby is any domestic cat with an 'M' on its forehead,
                             stripes by its eyes and across its cheeks.
                         </p>
                     </article>
-                    <article class="col-sm">
-                        <h3>Calico Cat</h3>
-                        <figure>
-                            <img class="img-thumbnail" src="Images/calico_small.jpg" alt="Callco"/>
-                            <figcaption>Calico</figcaption>
-                        </figure>
-                        <p>
-                            A calico cat is a domestic cat with a coat that is typically
-                            25% to 75% white and has large orange and black patches.
-                        </p>
-                    </article>
+
                 </div>
             </section>
-        </main>
-        <?php include "footer.inc.php" ?>
-    </body>
+</main>
+    <?php include "PHP/footer.inc.php" ?>
+</body> 
 </html>
